d9d7b14aff201ef6bfb262e0262f9c3549bd3e48
GakzAkk <gakzqwe@gmail.com>
2024-11-06 16:55:21 +0300

    Add files via upload

---
+++ scriptSpan.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        const spans = document.querySelectorAll('PIN_1730899337686_hd'); 
+        spans.forEach(span => span.remove());
+    }, 3000); 
+});
